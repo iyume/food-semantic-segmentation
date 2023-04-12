20fbd5fa04d4b96ce56dfc5507249c68f3018f4c
iyume <iyumelive@gmail.com>
2023-04-13 00:29:46 +0800

    refact evaluate

--- foodseg/evaluate.py
+++ foodseg/evaluate.py
@@ -1,50 +1,53 @@
 # scripts that should be run through `python -m foodseg.evaluate`
+from typing import ClassVar, Optional, cast
+
 import click
 import cv2
 import numpy as np
 import torch
-from torchvision import transforms
 
+from .datasets import GeneratedDataset
 from .models.unet import UNet
-from .utils import classes, id2color
+from .utils import State, classes, id2color
 
-device = torch.device("cpu")
-
-net = UNet(3, len(classes))
-net.to(device)
-net.eval()
-
-_transform = transforms.Compose([transforms.ToTensor()])
+transform = GeneratedDataset.transform
 
 
-def transform(cv_img: np.ndarray) -> torch.Tensor:
-    return _transform(cv_img)  # type: ignore
+class Evaluator:
+    model: ClassVar = UNet(3, len(classes))
+    model.eval()
 
+    def __init__(self, pth_file: Optional[str] = None, device: str = "cpu") -> None:
+        self.device = torch.device(device)
+        self.model.to(self.device)
+        if pth_file is not None:
+            state = cast(State, torch.load(pth_file, self.device))
+            self.model.load_state_dict(state["model_state_dict"])
 
-def evaluate(cv_img: np.ndarray) -> np.ndarray:
-    """(BGR) -> BGR."""
-    assert cv_img.ndim == 3
-    assert cv_img.shape[2] == 3
-    x = transform(cv_img)
-    x.unsqueeze_(0)
-    out: torch.Tensor = net(x)
-    out = out.detach().cpu().squeeze().numpy()
-    out = np.argmax(out, 0)
-    out_vis = np.zeros((out.shape[0], out.shape[1], 3), dtype=np.uint8)
-    for class_num, color in id2color.items():
-        out_vis[:, :, 0][out == class_num] = color[2]  # b channel
-        out_vis[:, :, 1][out == class_num] = color[1]  # g channel
-        out_vis[:, :, 2][out == class_num] = color[0]  # r channel
-    return out_vis
+    def evaluate(self, cv_img: np.ndarray) -> np.ndarray:
+        """(BGR) -> BGR."""
+        assert cv_img.ndim == 3
+        assert cv_img.shape[2] == 3
+        x = transform(cv_img)
+        x.unsqueeze_(0)
+        out: torch.Tensor = self.model(x)
+        out = out.detach().cpu().squeeze().numpy()
+        out = np.argmax(out, 0)
+        out_vis = np.zeros((out.shape[0], out.shape[1], 3), dtype=np.uint8)
+        for class_num, color in id2color.items():
+            out_vis[:, :, 0][out == class_num] = color[2]  # b channel
+            out_vis[:, :, 1][out == class_num] = color[1]  # g channel
+            out_vis[:, :, 2][out == class_num] = color[0]  # r channel
+        return out_vis
 
 
 @click.command()
-@click.argument("image", type=click.Path(exists=True, dir_okay=False))
+@click.argument("imgfile", type=click.Path(exists=True, dir_okay=False))
 @click.option("-p", "--pth-file", type=click.Path(exists=True, dir_okay=False))
 @click.option("-o", "--output", default="output.png", type=click.STRING)
-def cli(image: str, pth_file: str, output: str):
-    net.load_state_dict(torch.load(pth_file, device)["model_state_dict"])
-    out = evaluate(cv2.imread(image))
+def cli(imgfile: str, pth_file: str, output: str):
+    evaluator = Evaluator(pth_file=pth_file)
+    out = evaluator.evaluate(cv2.imread(imgfile))
     cv2.imwrite(output, out)
 
 
